0fae787c55297339573a50e6de9d0618e7570899
makunomark <makunomark@gmail.com>
2018-12-27 14:59:25 +0300

    mvp site

--- src/components/header.js
+++ src/components/header.js
@@ -2,31 +2,35 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Header = ({ siteTitle }) => (
+const Header = ({ siteTitle, activePage }) => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: 'rgb(250, 250, 250)',
+      borderBottom: '1px solid rgb(242, 242, 242)',
     }}
   >
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '1.2rem 1.0875rem',
+
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h4 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
+            color: 'rgb(102, 102, 102)',
             textDecoration: 'none',
+            fontWeight: 'normal',
           }}
         >
-          {siteTitle}
+          {siteTitle.toUpperCase()}
         </Link>
-      </h1>
+      </h4>
     </div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,72 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Tag, Avatar, Icon } from 'antd'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby s site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
+  <Layout activePage="home">
+    <SEO title="Home" keywords={['mark', 'makuno', 'gachoka']} />
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div
+        style={{
+          maxWidth: 700,
+          textAlign: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: 40,
+        }}
+      >
+        <Avatar size={150} icon="user" src={require('../images/marc.jpg')} />
+        <h1 style={{ marginTop: 12, fontWeight: 'normal' }}>Mark Makuno</h1>
+        <div>
+          <Tag color="geekblue">Software Engineer</Tag>
+          <Tag color="geekblue">Open source</Tag>
+          <Tag color="geekblue">Full Stack developer</Tag>
+        </div>
+        <p style={{ marginTop: 40 }}>
+          I am a Mobile &amp; Web architect, from Nairobi, Kenya. I specialize
+          in applications development. Currently, I work as a Software Engineer
+          at{' '}
+          <a href="https://twiga.ke" target="_blank">
+            Twiga Foods
+          </a>{' '}
+          Nairobi, Kenya.
+        </p>
+        <p style={{ marginTop: 12 }}>
+          My core skills are JavaScript (React, React Native, Next.js &amp;
+          NodeJs), Java &amp; XML. Want to know more? Drop me a line:
+          makunomark@gmail.com
+        </p>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/mark-makuno-188175100"
+            target="_blank"
+          >
+            <Icon
+              style={{ fontSize: 25, margin: 3, cursor: 'pointer' }}
+              type="linkedin"
+              theme="filled"
+            />
+          </a>
+          <a href="https://github.com/makunomark" target="_blank">
+            <Icon
+              style={{ fontSize: 25, margin: 3, cursor: 'pointer' }}
+              type="github"
+              theme="filled"
+            />
+          </a>
+          <a href="https://twitter.com/@thatmarc_" target="_blank">
+            <Icon
+              style={{ fontSize: 25, margin: 3, cursor: 'pointer' }}
+              type="twitter"
+            />
+          </a>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
